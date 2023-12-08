8d19038857a361e0890f8cf2b76cae443fd88c3e
iamvirgoo <carens.kha4atryan@yandex.ru>
2023-12-09 01:58:55 +0300

    FIX: todo

--- app/src/hooks/redux.ts
+++ app/src/hooks/redux.ts
@@ -1,5 +1,9 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "../store/store.ts";
+import { useDispatch, /*TypedUseSelectorHook*/ /*useSelector*/ } from "react-redux";
+import { AppDispatch, /*RootState*/ } from "../store/store.ts";
+
+/*TODO: какого-то хуя эта конструкция всегда работала, а сейчас не работает, пока не знаю схуя, но надо разобраться*/
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+/*export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;*/
+
+
