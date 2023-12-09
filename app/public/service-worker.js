908a4b4ee4c1688379e878fefb5dde0088ccf3d6
iamvirgoo <carens.kha4atryan@yandex.ru>
2023-12-10 02:49:36 +0300

    FIX: cache deleting

--- app/public/service-worker.js
+++ app/public/service-worker.js
@@ -14,6 +14,26 @@ self.addEventListener("install", (event) => {
         ]);
     })
 });
+
+self.addEventListener('activate', (event) => {
+    var expectedCacheNames = Object.keys(CACHE_NAME).map(function(key) {
+        return CACHE_NAME[key];
+    });
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (expectedCacheNames.indexOf(cacheName) === -1) {
+                        console.log('Deleting out of date cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    console.log('Service Worker has been activated');
+});
+
 self.addEventListener("fetch", (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
