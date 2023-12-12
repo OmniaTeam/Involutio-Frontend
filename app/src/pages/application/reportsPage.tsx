bf86be884089127507532a4179b291a65e8b7c9e
iamvirgoo <carens.kha4atryan@yandex.ru>
2023-12-12 19:51:21 +0300

    FIX: dropdown menu component

--- app/src/pages/application/reportsPage.tsx
+++ app/src/pages/application/reportsPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { motion } from "framer-motion";
 import { useAppSelector } from "../../hooks/redux.ts";
 import { EUserRole } from "../../models/EUserRole.ts";
