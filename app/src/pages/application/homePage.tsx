cbbce8f86096400685be9ed3f97d6f7a727f7661
iamvirgoo <carens.kha4atryan@yandex.ru>
2023-12-14 00:09:09 +0300

    FIX: home page for manager styles

--- app/src/pages/application/homePage.tsx
+++ app/src/pages/application/homePage.tsx
@@ -17,12 +17,14 @@ export default function HomePage() {
 			{USER.role === EUserRole.manager
 				? <>
 					<motion.h2
+						className={'home--content__title'}
 						initial={{ opacity: 0 }}
 						animate={{ opacity: 1 }}
 						transition={{ delay: 0.1, duration: 0.5 }}
-					>Привет, {USER.fio}</motion.h2>
+					>Привет, <strong>{USER.fio}</strong></motion.h2>
 					<div className={'statistic'} style={{
-						maxWidth: "100%"
+						maxWidth: "100%",
+						marginBottom: "0"
 					}}>
 						<motion.div
 							className={'statistic--path'}
@@ -30,7 +32,7 @@ export default function HomePage() {
 							animate={{ opacity: 1 }}
 							transition={{ delay: 0.1, duration: 0.5 }}
 						>
-							<Link to={'/application/employees'}>Посмотреть своих сотрудников</Link>
+							<Link to={'/application/employees'} style={{color: "#FFFFFF"}}>Посмотреть своих сотрудников</Link>
 						</motion.div>
 						<motion.div
 							className={'statistic--path'}
@@ -38,7 +40,7 @@ export default function HomePage() {
 							animate={{ opacity: 1 }}
 							transition={{ delay: 0.1, duration: 0.5 }}
 						>
-							<Link to={'/application/employees'}>Посмотреть отчёты отдела</Link>
+							<Link to={'/application/employees'} style={{color: "#FFFFFF"}}>Посмотреть отчёты отдела</Link>
 						</motion.div>
 					</div>
 				</>
