06838e3d7bd88ff4b86ebba86f99144664277f7f
iamvirgoo <carens.kha4atryan@yandex.ru>
2023-12-14 00:19:20 +0300

    WIP: home page for admin

--- app/src/pages/application/homePage.tsx
+++ app/src/pages/application/homePage.tsx
@@ -1,12 +1,11 @@
-import { motion } from "framer-motion"
-import { useAppSelector } from "../../hooks/redux.ts";
-import { Link } from "react-router-dom";
-import { EUserRole } from "../../models/EUserRole.ts";
+import {motion} from "framer-motion"
+import {useAppSelector} from "../../hooks/redux.ts";
+import {Link} from "react-router-dom";
+import {EUserRole} from "../../models/EUserRole.ts";
 
 export default function HomePage() {
 	const USER = useAppSelector((state) => state.user)
 
-	//TODO: написать что-то привественное для админа и менеджера
 	return <div className={'home'}>
 		<motion.h2 className={'home--title'}
            initial={{ opacity: 0 }}
@@ -32,7 +31,8 @@ export default function HomePage() {
 							animate={{ opacity: 1 }}
 							transition={{ delay: 0.1, duration: 0.5 }}
 						>
-							<Link to={'/application/employees'} style={{color: "#FFFFFF"}}>Посмотреть своих сотрудников</Link>
+							<Link to={'/application/employees'} style={{color: "#FFFFFF"}}>
+								Посмотреть своих сотрудников</Link>
 						</motion.div>
 						<motion.div
 							className={'statistic--button'}
@@ -40,11 +40,54 @@ export default function HomePage() {
 							animate={{ opacity: 1 }}
 							transition={{ delay: 0.1, duration: 0.5 }}
 						>
-							<Link to={'/application/employees'} style={{color: "#FFFFFF"}}>Посмотреть отчёты отдела</Link>
+							<Link to={'/application/reports'} style={{color: "#FFFFFF"}}>
+								Посмотреть отчёты отдела</Link>
 						</motion.div>
 					</div>
 				</>
-				: <></>
+				: <>{USER.role === EUserRole.admin
+					? <>
+						<motion.h2
+							className={'home--content__title'}
+							initial={{opacity: 0}}
+							animate={{opacity: 1}}
+							transition={{delay: 0.1, duration: 0.5}}
+						>Привет, <strong>{USER.fio}</strong></motion.h2>
+						<div className={'statistic'} style={{
+							maxWidth: "100%",
+							marginBottom: "0"
+						}}>
+							<motion.div
+								className={'statistic--button'}
+								initial={{opacity: 0}}
+								animate={{opacity: 1}}
+								transition={{delay: 0.1, duration: 0.5}}
+							>
+								<Link to={'/application/departments'} style={{color: "#FFFFFF"}}>
+									Посмотреть список отделов</Link>
+							</motion.div>
+							<motion.div
+								className={'statistic--button'}
+								initial={{opacity: 0}}
+								animate={{opacity: 1}}
+								transition={{delay: 0.1, duration: 0.5}}
+							>
+								<Link to={'/application/employees'} style={{color: "#FFFFFF"}}>
+									Посмотреть сотрудников отделений</Link>
+							</motion.div>
+							<motion.div
+								className={'statistic--button'}
+								initial={{opacity: 0}}
+								animate={{opacity: 1}}
+								transition={{delay: 0.1, duration: 0.5}}
+							>
+								<Link to={'/application/reports'} style={{color: "#FFFFFF"}}>
+									Посмотреть отчёты по отделениям</Link>
+							</motion.div>
+						</div>
+					</>
+					: <></>
+				}</>
 			}
 		</div>
 	</div>
